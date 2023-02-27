994e3bc8535ca58fe099c0778e6f1b1bb88a6e61
desireemenezes <deemenezes@hotmail.com>
2023-02-27 19:21:13 -0300

    add dark theme :lipstick:

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,16 @@
 import { Button } from '@mui/material';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
+
+import { useAppThemeContext } from '../shared/contexts';
 
 export const AppRoutes = () => {
+  const { toggleTheme } = useAppThemeContext();
 
   return (
     <Routes>
-      <Route path="/pagina-inicial" element={<p>Página inicial</p>} />
+      <Route path="/pagina-inicial" element={<Button variant='contained' color='primary' onClick={toggleTheme}>Toggle theme</Button>} />
 
-      <Route path="/pagina-inicial" element={<Button variant='contained' color='primary'>Teste</Button>} />
+      <Route path="*" element={<Navigate to="/pagina-inicial" />} />
     </Routes>
   );
 }
