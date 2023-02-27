70ced8727620cace8fa1f157c2a9a24003d0e62a
desireemenezes <deemenezes@hotmail.com>
2023-02-27 19:10:18 -0300

    Create theme @mui :lipstick:

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
+import { ThemeProvider } from '@mui/material';
+import { BrowserRouter } from "react-router-dom";
+
+import { LightTheme } from './shared/themes';
+import { AppRoutes } from './routes';
+
 export const App = () => {
   return (
-    <div className="App">
-      Teste
-    </div>
+    <ThemeProvider theme={LightTheme}>
+      <BrowserRouter>
+        <AppRoutes />
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
