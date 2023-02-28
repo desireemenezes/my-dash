02fca756b4301a8cb6cce14fe2b827d057a8f0d0
desireemenezes <deemenezes@hotmail.com>
2023-02-28 15:41:20 -0300

    feat: config menu :wrench:

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,21 @@
-import { Button } from '@mui/material';
+import { useEffect } from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
+import { Button } from '@mui/material';
 
 import { useDrawerContext } from '../shared/contexts';
 
 export const AppRoutes = () => {
-  const { toggleDrawerOpen } = useDrawerContext();
+  const { toggleDrawerOpen, setDrawerOptions } = useDrawerContext();
+
+  useEffect(() => {
+    setDrawerOptions([
+      {
+        icon: 'home',
+        path: '/pagina-inicial',
+        label: 'Página inicial',
+      },
+    ]);
+  }, []);
 
   return (
     <Routes>
@@ -12,5 +23,5 @@ export const AppRoutes = () => {
 
       <Route path="*" element={<Navigate to="/pagina-inicial" />} />
     </Routes>
-  );
+  ); 
 };
